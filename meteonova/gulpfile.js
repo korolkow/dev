7600e173a17090c155ec1fd8773d9449a196685f
alekseyvasilev <alexv@gismeteo.ru>
2019-12-19 21:00:14 +0300

    fix

--- meteonova/gulpfile.js
+++ meteonova/gulpfile.js
@@ -3,6 +3,11 @@
 var gulp = require('gulp'),
     rename = require('gulp-rename'),
     inject = require('gulp-inject'),
+    cssbeautify = require('gulp-cssbeautify'),
+    cssconcat = require('gulp-concat-css'),
+    cssnano = require('gulp-cssnano'),
+    beautify = require('gulp-jsbeautifier'),
+    uglify = require('gulp-uglifyjs'),
     prettify = require('gulp-html-prettify'),
     //convertEncoding = require('gulp-convert-encoding'),
     gulpSSH = require('gulp-ssh'),
@@ -303,14 +308,13 @@ dt = dt.getFullYear() + '' + addZero(dt.getMonth() + 1) + '' + addZero(dt.getDat
 gulp.task('addCss', function () {
     return gulp.src(css)
         .pipe(rename({dirname: ''}))
-        .pipe(gulp.dest('css/'))
+        .pipe(cssbeautify())
         .pipe(gulpSSH.dest('/home/nova/dev/meteonova/css'))
 })
 
 gulp.task('addJs', function () {
     return gulp.src(js)
         .pipe(rename({dirname: ''}))
-        .pipe(gulp.dest('js/'))
         .pipe(gulpSSH.dest('/home/nova/dev/meteonova/js'))
 })
 
@@ -345,7 +349,59 @@ gulp.task('d', gulp.series('build_dev', async function () {
     gulp.src('build/dev/*.htm').pipe(gulpSSH.dest('/home/nova/dev/meteonova'))
 }))
 
-//gulp.task('prod', gulp.series('build_prod', async function () {
+
+gulp.task('addCssMin', function () {
+    return data.map(function(page) {
+        var injectCss = commonScripts.css.concat(page.css);
+        return gulp.src(injectCss)
+            .pipe(rename({dirname: ''}))
+            .pipe(cssbeautify())
+            .pipe(cssconcat('meteonova.'+ page.tpl +'.min.css'))
+            .pipe(cssnano())
+            .pipe(gulpSSH.dest('/home/nova/dev/meteonova/css'))
+    })
+})
+
+gulp.task('addJsMin', function () {
+    return data.map(function(page) {
+        var inject = commonScripts.js.concat(page.js);
+        return gulp.src(inject)
+            .pipe(rename({ dirname: '' }))
+            .pipe(beautify())
+            .pipe(concat('meteonova.'+ page.tpl +'.min.js'))
+            .pipe(uglify())
+            .pipe(gulpSSH.dest('/home/nova/dev/meteonova/js'))
+    })
+})
+
+gulp.task('build_prod', gulp.series(['addCssMin', 'addJsMin'], async function () {
+    return data.map(function(page) {
+        var injectCss = commonScripts.css.concat(page.css);
+        var injectJs = commonScripts.js.concat(page.js);
+        return gulp.src(page.tpl+'.*')
+            .pipe(inject(gulp.src(injectCss), {
+                transform: function (filepath) {
+                    return '<link rel="stylesheet" type="text/css" href="<#CSSBASE>' +
+                        filepath.replace(/(.+\/css)/, '') + '?' + dt + '">'
+                }
+            }))
+            .pipe(inject(gulp.src(injectJs), {
+                transform: function (filepath) {
+                    return '<script type="text/javascript" src="<#JSBASE>' +
+                        filepath.replace(/(.+\/js)/, '') + '?' + dt + '" charset = "utf-8"></script>'
+                }
+            }))
+            .pipe(replace('style="width:222px;"', ''))
+            .pipe(replace('style="width:222px"', ''))
+            .pipe(replace(/<div.(class="block_bt.*").*><\/div>/gm, ''))
+            .pipe(replace(/<div class="round_(left|right)">\s*<img\s+src=\"\/images\/.*.png\"\s*.*(class="corner")\s*\/*>\s*<\/div>/gm, ''))
+            .pipe(replace(/<div class="round_(left|right)">\s*<img\s+src=\"<#IMGBASE>\/.*.png\"\s*.*(class="corner")\s*\/*>\s*<\/div>/gm, ''))
+            .pipe(prettify())
+            .pipe(gulp.dest('build/dev'));
+    })
+}))
+
+gulp.task('prod', gulp.series('build_prod', async function () {
     //gulp.src('build/prod/*.html').pipe(gulp.dest('/'))
-//})))
+}))
 
--- meteonova/js/inc/jsengine.php
+++
@@ -1,8 +0,0 @@
-<?PHP
-header("content-type: text/javascript");
-$tpl=$_GET["tpl"];
-$tpls = array("main" => "styles,hints,tools", "frc" => "styles,ajax,graph,hints,tools,transparent,weather,scroll", "agro" => "styles,ajax,hints,tools,transparent,agro", "auto" => "styles,ajax,hints,tools,transparent,weather2,scroll", "avia" => "styles,ajax,hints,tools,transparent,weather3,scroll", "med" => "styles,ajax,hints,tools,transparent,weather3,scroll", "pro" => "styles,ajax,hints,tools,transparent", "month" => "styles,ajax,hints,tools,transparent,scroll");
-$incs=split(",",$tpls[$tpl]);
-$c=count($incs);
-if($c>0) for($i=0; $i<$c; $i++){ include $incs[$i].".inc" ;}
-?>
